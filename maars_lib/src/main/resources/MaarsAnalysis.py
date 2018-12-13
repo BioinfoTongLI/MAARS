ee57723599272fb957bb83b6ed50b33b397012c4
tongli <tongli.bioinfo@gmail.com>
2018-12-13 17:19:16 +0100

    adapt to the Analyser

--- maars_lib/src/main/resources/MaarsAnalysis.py
+++ maars_lib/src/main/resources/MaarsAnalysis.py
@@ -1,40 +1,40 @@
 # coding: utf-8
 # !/usr/bin/env python3
-import os
-import sys
 
-from maarsanalyzer import Plotting
+from maarsanalyzer.classification.maarsClassic import is_in_mitosis
 from maarsanalyzer.MaarsAnalyzer import MaarsAnalyzer
-sys.path.append(os.path.abspath(".."))
 from maarsanalyzer.analysers import ComputeFeatures
 from maarsanalyzer.io_helpers import Loader
-from maarsanalyzer.io_helpers.Consts import Constants
 import pandas as pd
 from pathlib import Path
+import numpy as np
 idx = pd.IndexSlice
 
 if __name__ == '__main__':
+
     args = Loader.set_attributes_from_cmd_line()
     root = Path(args.baseDir)
     params = Loader.load_parameters(root)
     analyser = MaarsAnalyzer(params, pos=args.pos, calibration=args.calibration, interval=args.interval)
+    analyser.cell_rois_features.to_hdf(analyser.csts.MITO_DIR / "roi_features.h5", key="roi_features")
 
     spots = Loader.load_all_spots(analyser.csts.FLUO_SPOTS).swaplevel(0, 2).sort_index()
     spots.index.names = ["TimePoint", "Channel", "Cell", "ID_dot"]
     spots.to_hdf(analyser.csts.MITO_DIR / "original_spots.h5", key="spots")
-
+    print("Spots saved.")
     # spots = pd.read_hdf(analyser.csts.MITO_DIR / "original_spots.h5")
-    # spots.index.names = ["TimePoint", "Channel", "Cell", "dot_id"]
 
     xy = spots[["POSITION_X", "POSITION_Y"]].astype(np.float)
-    gb_t_cell = xy.groupby(["TimePoint", "Cell"])
-    geos = gb_t_cell.apply(ComputeFeatures.evaluate, roi_features=analyser.cell_rois_features)
+    gb_names = ["TimePoint", "Cell"]
+    gb_t_cell = xy.groupby(gb_names)
+    geos = analyser.apply_parallel(gb_t_cell, ComputeFeatures.evaluate)
+    geos.index.names = gb_names
+
     geos.to_hdf(analyser.csts.MITO_DIR / "geos.h5", key="geos")
+    print("Geos saved.")
+    # geos = pd.read_hdf(analyser.csts.MITO_DIR / "geos.h5")
 
-    geos = pd.read_hdf(analyser.csts.MITO_DIR / "geos.h5")
-    # print(geos.columns)
-
-    sp_lens = geos["sp_len"].unstack(level=[1])
+    sp_lens = geos["sp_len"].unstack(level=[1]).astype(np.float)
 
     cells_has_sp_mask = sp_lens.apply(lambda one_series: one_series.any())
     cells_has_sp_mask.to_hdf(analyser.csts.MITO_DIR / "has_sp_mask.h5", key="has_sp_mask")
@@ -43,18 +43,15 @@ if __name__ == '__main__':
                                    p=analyser.csts.DYNAMIC_P_THRESHOLD,
                                    min_seg_len=analyser.csts.MIN_SEG_LEN)
     sp_mito_filter.to_hdf(analyser.csts.MITO_DIR / "mitosis_mask.h5", key="mitosis")
+    print("Mitosis mask saved")
 
     mito_elong = sp_lens.loc[:, sp_mito_filter]
-    # plt.plot(mito_elong)
-    # plt.show()
 
-    kt_area = geos["kt_area"].unstack(level=[1])
+    kt_area = geos["kt_area"].unstack(level=[1]).astype(np.float)
     exist_kt_area = kt_area.apply(lambda one_series: one_series.any())
     exist_kt_area.to_hdf(analyser.csts.MITO_DIR / "has_kt_cloud.h5", key="has_kt_cloud")
-
+    print("Kt cloud mask saved.")
     kt_area_data = sp_lens.loc[:, exist_kt_area]
-    plt.plot(kt_area_data)
-    plt.show()
 
     has_kt_area_and_sp_elong = exist_kt_area & sp_mito_filter
     has_kt_area_and_sp_elong.to_hdf(analyser.csts.MITO_DIR / "has_kt_cloud_and_mitosis.h5",
@@ -63,6 +60,9 @@ if __name__ == '__main__':
     has_kt_area_or_sp_elong = exist_kt_area | sp_mito_filter
     has_kt_area_or_sp_elong.to_hdf(analyser.csts.MITO_DIR / "has_kt_area_or_sp_elong.h5",
                                    key="has_kt_area_or_sp_elong")
+
+    # plt.plot(sp_lens.loc[:, has_kt_area_or_sp_elong])
+    # plt.show()
 
     # Plotting.plot_elong(sp_lens, analyser.csts.MITO_DIR)
 
